38df7e24f46b753c5ec0735d4a1500b0ee80ef0a
Marco Haber <marcoshaber99@gmail.com>
2023-09-01 18:17:38 +0300

    intro section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Intro from '@/components/intro'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative`}
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]">
         </div>
